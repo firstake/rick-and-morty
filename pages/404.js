75039f83d33c349ed014e5a1e48a1b5cf1bb6af3
Vladislav Tereshkin <firstaake@gmail.com>
2020-04-19 20:57:45 +0300

    add <head> for 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 import { global } from '../components/Header/HeaderStyles';
 import BackwardLink from '../components/BackwardLink';
 
 const Custom404 = () => (
   <React.Fragment>
+    <Head>
+      <title>Rick and Morty - Nothing Found</title>
+      <link rel="icon" href="/favicon.png" />
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet" />
+    </Head>
     <h1>404 | Nothing found...</h1>
     <div>
       <BackwardLink pattern="/" to="/" />
